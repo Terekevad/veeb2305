3a2199e9f732aa2ed1d8ce0119e5167b3562d5a1
Terekevad <130385252+Terekevad@users.noreply.github.com>
2023-06-12 21:25:42 +0300

    loeng03

--- loeng02/client/script.js
+++ loeng02/client/script.js
@@ -7,13 +7,15 @@ fetch('http://localhost:8000')
     .then((data) => {
         console.log('õnnestus', data);
         for (let matk of data) {
+
             console.log(matk);
             app.innerHTML += `
         <div class="matk">
         <h2>${matk.title}</h2>
-        <div>Algusaeg: ${matk.startTime}</div>
+        <div>Start: ${matk.startTime}</div>
         <div>${matk.description}</div>
         <a href="https://www.google.com/maps/@${matk.lat},${matk.long},17z" target="_blank">Koguneme siin!</a><br>
+        <div>Osalejad: ${matk.participants}</div>
         <img src="${matk.imageUrl}">
         </div>
         `;
--- loeng02/server/index.js
+++ loeng02/server/index.js
@@ -7,20 +7,20 @@ let matk1 = {
     title: 'Matk Taevaskojas', 
     imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/0e/4f/f3/1e/taevaskoja-salamaa.jpg',
     description: 'Jalga panna mugavad jalanõud!',
-    startTime: new Date(2023, 7, 1, 08, 15, 0),
+    startTime: '01.08.2023 kell 08:30',
     long: 27.0286815,
     lat: 58.1057632,
-    participants: ['Karoli']
+    participants: ['Karoli' , 'Merili' , 'Peeter']
 }
 
 let matk2 = {
     title: 'Kanuumatk Võhandu jõel', 
     imageUrl: 'https://lounaeestlane.ee/wp-content/uploads/2020/07/13224013t1ha3f5.jpg',
     description: 'Võtame kanuud Võrust Tamula rannast ning lõpetame Taevaskojas',
-    startTime: new Date(2023, 6, 1, 09, 0, 0),
+    startTime: '01.07.2023 kell 08:00',
     long: 26.99138,
     lat: 57.84335,
-    participants: ['Karoli']
+    participants: ['Lauri' , 'Riho' , 'Helerin']
 }
 
 let matkad = [matk1, matk2]

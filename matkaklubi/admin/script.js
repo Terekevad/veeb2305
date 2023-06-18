0cbddf4da762a67d962a2d0e8e8cc43d62c61a58
Terekevad <130385252+Terekevad@users.noreply.github.com>
2023-06-19 00:41:16 +0300

    matkaklubi

--- matkaklubi/admin/script.js
+++ matkaklubi/admin/script.js
@@ -1,26 +1,33 @@
-  //const API_URL = 'https://karoli-matkad-app.onrender.com';
-  const API_URL = 'http://localhost:10000';
-  const matkad = document.querySelector('#matkad');
-  const uusMatk = document.querySelector('#uus-matk');
-  const uusMatkNimi = document.querySelector('#uus-matk-nimi');
-  const uusMatkLaiuskraad = document.querySelector('#uus-matk-laiuskraad');
-  const uusMatkPikkuskraad = document.querySelector('#uus-matk-pikkuskraad');
-  const uusMatkHind = document.querySelector('#uus-matk-hind');
-  const uusMatkPildiUrl = document.querySelector('#uus-matk-pildi-url');
-  const uusMatkAlgus = document.querySelector('#uus-matk-algus');
-  const uusMatkLopp = document.querySelector('#uus-matk-lopp');
-  const uusMatkKirjeldus = document.querySelector('#uus-matk-kirjeldus');
-  matkad.innerHTML = 'Loading...'
+//const API_URL = 'https://karoli-matkad-app.onrender.com';
+const API_URL = 'http://localhost:10000';
+const matkad = document.querySelector('#matkad');
+const uusMatk = document.querySelector('#uus-matk');
+const uusMatkNimi = document.querySelector('#uus-matk-nimi');
+const uusMatkLaiuskraad = document.querySelector('#uus-matk-laiuskraad');
+const uusMatkPikkuskraad = document.querySelector('#uus-matk-pikkuskraad');
+const uusMatkHind = document.querySelector('#uus-matk-hind');
+const uusMatkPildiUrl = document.querySelector('#uus-matk-pildi-url');
+const uusMatkAlgus = document.querySelector('#uus-matk-algus');
+const uusMatkLopp = document.querySelector('#uus-matk-lopp');
+const uusMatkKirjeldus = document.querySelector('#uus-matk-kirjeldus');
+matkad.innerHTML = 'Loading...'
 
-  function startCreatingNewTrek() {
-      uusMatk.style.display = 'flex';
-  }
+function startCreatingNewTrek() {
+    uusMatk.style.display = 'flex';
+    emptyAllInputs();
+}
 
-  function closeNewTrekPanel() {
-      uusMatk.style.display = 'none';
-  }
+function editTrek() {
+    console.log('edit trek kutsuti välja');
+    uusMatkNimi.value = 'mingi asi';
+}
 
-  async function addTrek() {
+function closeNewTrekPanel() {
+    uusMatk.style.display = 'none';
+    emptyAllInputs();
+}
+
+async function addTrek() {
     if (!isAnyInputEmpty()) {
         return;
     }
@@ -42,64 +49,66 @@
             })
 
         }).then((response => response.json()));
-        console.log(treks);
-        matkad.innerHTML = `
-        <h2>Matkad</h2>
-        `;
-        for (let trek of treks.rows.rows) {
-            matkad.innerHTML = `
-                <a href="">${trek.name}</a>
-            `;
-        }
-        matkad.innerHTML += `
-             <a onclick="startCreatingNewTrek()" href="javascript:;">Lisa uus matk</a>
-        `;
+
+        getTreks();
+        emptyAllInputs();
     } catch (e) {
         console.log(e);
         matkad.innerHTML = 'Error, couldn\´t fetch treks';
     }
 
-  }
-
-  async function getTreks() {
-      try {
-          const treks = await fetch(`${API_URL}/api/treks`).then((response => response.json()));
-          console.log(treks);
-          matkad.innerHTML = `
-          <h2>Matkad</h2>
-          `;
-          for (let trek of treks.rows.rows) {
-              matkad.innerHTML = `
-                  <a href="">${trek.name}</a>
-              `;
-          }
-          matkad.innerHTML += `
-               <a onclick="startCreatingNewTrek()" href="javascript:;">Lisa uus matk</a>
-          `;
-      } catch (e) {
-          console.log(e);
-          matkad.innerHTML = 'Error, couldn\´t fetch treks';
-      }
-  }
-
-  function isAnyInputEmpty() {
-    if (
-    uusMatkNimi.value === '' || 
-    uusMatkLaiuskraad.value === '' ||
-    uusMatkPikkuskraad.value === '' ||
-    uusMatkHind.value === '' ||
-    uusMatkPildiUrl.value === '' ||
-    uusMatkAlgus.value === '' ||
-    uusMatkLopp.value === '' ||
-    uusMatkKirjeldus.value === ''
-  ) {
-    return true;
-  }
-  return false;
 }
 
-document.getElementById("lisa-matk-nupp").addEventListener("click", function(event) {
+async function getTreks() {
+    try {
+        const treks = await fetch(`${API_URL}/api/treks`).then((response => response.json()));
+        console.log(treks);
+        matkad.innerHTML = `
+          <h2>Matkad</h2>
+          `;
+        for (let trek of treks.rows.rows) {
+            matkad.innerHTML = `
+                  <a onclick="editTrek()" href="javascript:;">${trek.name}</a>
+              `;
+        }
+        matkad.innerHTML += `
+               <a onclick="startCreatingNewTrek()" href="javascript:;">Lisa uus matk</a>
+          `;
+    } catch (e) {
+        console.log(e);
+        matkad.innerHTML = 'Error, couldn\´t fetch treks';
+    }
+}
+
+function isAnyInputEmpty() {
+    if (
+        uusMatkNimi.value === '' ||
+        uusMatkLaiuskraad.value === '' ||
+        uusMatkPikkuskraad.value === '' ||
+        uusMatkHind.value === '' ||
+        uusMatkPildiUrl.value === '' ||
+        uusMatkAlgus.value === '' ||
+        uusMatkLopp.value === '' ||
+        uusMatkKirjeldus.value === ''
+    ) {
+        return true;
+    }
+    return false;
+}
+
+function emptyAllInputs() {
+    uusMatkNimi.value = '';
+    uusMatkLaiuskraad.value = '';
+    uusMatkPikkuskraad.value = '';
+    uusMatkHind.value = '';
+    uusMatkPildiUrl.value = '';
+    uusMatkAlgus.value = '';
+    uusMatkLopp.value = '';
+    uusMatkKirjeldus.value = '';
+}
+
+document.getElementById("lisa-matk-nupp").addEventListener("click", function (event) {
     event.preventDefault();
 });
 
-  getTreks();
+getTreks();
